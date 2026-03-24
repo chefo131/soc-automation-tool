6e0150444a8a778ab00b9f07c80e02580e21ec2e
chefo131 <tiriri.131@gmail.com>
2026-03-24 09:21:07 +0100

    Ajustar base path para soc-automation-tool

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/soc-automation-generator/",
+  base: "/soc-automation-tool/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
